97bfe2b1fd479f0a6d73f4e486b360424af61e06
Okoro21 <chrisokoro21@gmail.com>
2025-05-16 16:30:20 -0700

    publish heading error

--- ugv_final/ugv_final/ugv_heading_ctrl.py
+++ ugv_final/ugv_final/ugv_heading_ctrl.py
@@ -101,6 +101,7 @@ class loggerSubscriber:
                         heading_error = round(heading_error, 3) #Three 3 places of precisions
                         print(heading_error)
                         xsens_obj.heading_error = heading_error
+                        writer.write(xsens_obj)
                 # Dispatch will call the handlers associated to the WaitSet conditions
                 # when they activate
                 else:
