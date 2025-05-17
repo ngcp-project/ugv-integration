c47fcf3e4e05fa54f9ee231a0e3714e70b91f7b6
Okoro21 <chrisokoro21@gmail.com>
2025-05-16 17:57:59 -0700

    Modified to process heading error

--- ugv_final/ugv_final/ugv_drive_stm.py
+++ ugv_final/ugv_final/ugv_drive_stm.py
@@ -58,9 +58,15 @@ class UgvControlSub:
         STEER_CMD = 0
         if(len(samples) != None): 
             for sample in samples:
-                print(f"Received: {sample}")
-                heading_error = sample.heading_error
-                print(f"Heading error: {heading_error}")
+                if sample.auto_en == True:
+                    print(f"Received: {sample}")
+                    heading_error = sample.heading_error
+                    print(f"Heading error: {heading_error}")
+                    auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
+                    udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
+                    server_socket.sendto(udp_payload, (client_ip, client_port))
+                    print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
+                    
             print("Autonomous Enabled")
             if samples[0].auto_en == True:
                 # AUTO_VEL = -1.0
