c8f86053cd926ab1acf23293235d4987a3b9f3b2
Okoro21 <chrisokoro21@gmail.com>
2025-05-16 17:54:56 -0700

    Modified how I am reading data from the app

--- ugv_final/ugv_final/ugv_drive_stm.py
+++ ugv_final/ugv_final/ugv_drive_stm.py
@@ -54,20 +54,24 @@ class UgvControlSub:
     @staticmethod
     def auto_process_data(auto_reader):
         samples = auto_reader.take_data()
+        AUTO_VEL = -1.0
+        STEER_CMD = 0
         if(len(samples) != None): 
             for sample in samples:
                 print(f"Received: {sample}")
+                heading_error = sample.heading_error
+                print(f"Heading error: {heading_error}")
             print("Autonomous Enabled")
             if samples[0].auto_en == True:
-                AUTO_VEL = -1.0
-                STEER_CMD = 0
+                # AUTO_VEL = -1.0
+                # STEER_CMD = 0
                 heading_error = samples[0].heading_error 
                 print(f"Object distance of depth Camera {heading_error}")
-                print(f"Heading error: {samples[0].heading_error}")
-                auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
-                udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
-                server_socket.sendto(udp_payload, (client_ip, client_port))
-                print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
+                # print(f"Heading error: {samples[0].heading_error}")
+                # auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
+                # udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
+                # server_socket.sendto(udp_payload, (client_ip, client_port))
+                # print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
             print("Getting Data from Autonomous Topic")
         
         else: 
