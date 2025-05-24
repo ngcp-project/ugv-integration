055a22e5960af2438843a6100b5379602cdd9569
Okoro21 <chrisokoro21@gmail.com>
2025-05-24 01:59:23 -0700

    Modified ugv drive to receive heading data over udp

--- ugv_final/ugv_final/ugv_drive_stm.py
+++ ugv_final/ugv_final/ugv_drive_stm.py
@@ -53,37 +53,47 @@ class UgvControlSub:
     
     @staticmethod
     def auto_process_data(auto_reader):
+        heading_error_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        heading_address = ("localhost", 33333)
+        heading_error_server.bind(heading_address)
+        heading_error, _ = heading_error_server.recvfrom(1024) 
+        heading_error = float(heading_error.decode())
+
         samples = auto_reader.take_data()
         AUTO_VEL = -1.0
         STEER_CMD = 0
-        if(len(samples) != None): 
-            for sample in samples:
-                if sample.auto_en == True:
-                    print(f"Received: {sample}")
-                    heading_error = sample.heading_error
-                    print(f"Heading error: {heading_error}")
-                    auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
-                    udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
-                    server_socket.sendto(udp_payload, (client_ip, client_port))
-                    print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
+        udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
+        server_socket.sendto(udp_payload, (client_ip, client_port))
+        print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
+
+        # if(len(samples) != None): 
+        #     for sample in samples:
+        #         if sample.auto_en == True:
+        #             print(f"Received: {sample}")
+        #             heading_error = sample.heading_error
+        #             print(f"Heading error: {heading_error}")
+        #             auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
+        #             udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
+        #             server_socket.sendto(udp_payload, (client_ip, client_port))
+        #             print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
                     
-            print("Autonomous Enabled")
-            if samples[0].auto_en == True:
-                # AUTO_VEL = -1.0
-                # STEER_CMD = 0
-                heading_error = samples[0].heading_error 
-                print(f"Object distance of depth Camera {heading_error}")
-                # print(f"Heading error: {samples[0].heading_error}")
-                # auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
-                # udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
-                # server_socket.sendto(udp_payload, (client_ip, client_port))
-                # print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
-            print("Getting Data from Autonomous Topic")
+        #     print("Autonomous Enabled")
+        #     if samples[0].auto_en == True:
+        #         # AUTO_VEL = -1.0
+        #         # STEER_CMD = 0
+        #         heading_error = samples[0].heading_error 
+        #         print(f"Object distance of depth Camera {heading_error}")
+        #         # print(f"Heading error: {samples[0].heading_error}")
+        #         # auto_flag = float(samples[0].auto_en)  # Convert boolean flag to float so that it can be properly decoded on the nucleo side
+        #         # udp_payload = f"{AUTO_VEL}, {STEER_CMD}, {auto_flag}, {heading_error}".encode()
+        #         # server_socket.sendto(udp_payload, (client_ip, client_port))
+        #         # print(f"Const Vel: {AUTO_VEL}, Const Steer: {STEER_CMD}, Autonomous Flag: {auto_flag}: heading error: {heading_error}")
+        #     print("Getting Data from Autonomous Topic")
         
-        else: 
-            ("Autonomous Buffer is empty")
+        # else: 
+        #     ("Autonomous Buffer is empty")
         
-        return len(samples)
+        # return len(samples)
 
 
     @staticmethod
